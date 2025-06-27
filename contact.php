34473b8921a157eb497f594a5df4a2902e009d45
Zaher ABBAS <z.abbas83@yahoo.com>
2025-06-27 09:26:46 +0200

    Enhance `contact.php` and `traitement.php` with form validation.

--- contact.php
+++ contact.php
@@ -11,26 +11,27 @@
         <h1>Contact</h1>
         <br>
         <form action="traitement.php" method="post" enctype="application/x-www-form-urlencoded">
-            <label for="name">Name:*</label>
+            <label for="name">Votre Nom* :</label>
             <input type="text" name="name" id="name" maxlength="20" required>
             <br>
             <br>
-            <label for="email">Email:*</label>
+            <label for="email">Votre Email* :</label>
             <input type="email" name="email" id="email" pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-                   required>
+                   required
+            >
             <br>
             <br>
-            <label for="message">Message:</label>
+            <label for="message">Votre Message* :</label>
             <br>
-            <textarea name="message" id="message" cols="30" rows="10" placeholder="Enter your message here!"
+            <textarea name="message" id="message" cols="30" rows="10" placeholder="Enter your message here!" required
             ></textarea>
             <br>
             <br>
-            <input type="checkbox" name="cgu" id="cgu" value="yes" required>
-            <label for="cgu">I agree to the terms and conditions</label>
+            <input type="checkbox" name="cgu" id="cgu" value="consent" required>
+            <label for="cgu">Accepter les conditions générales d'utilisation</label>
             <br>
             <br>
-            <input type="submit" value="Submit">
+            <input type="submit" value="Envoyer">
         </form>
     </section>
 </main>
--- traitement.php
+++ traitement.php
@@ -1,10 +1,65 @@
 <?php
 
-$name = $_POST["name"];
-$message = $_POST["message"];
+//Validation et récuperation des données du formulaire
 
-echo "Merci $name pour votre message!" . PHP_EOL;
+$errors = [];
 
-if ($message == "") {
-    echo "Le message est vide" . PHP_EOL;
+//Cleaning with trim (deleting spaces) and htmlentities to prevent les failles XSS
+//$_Post is an associative array and superglobal variable (every value is associated to its name in the form name attribute
+
+$data = array_map('trim', $_POST);
+$dataFinal = array_map('htmlentities', $data);
+
+//Validation name
+if (!isset($dataFinal["name"]) || empty($dataFinal["name"]))
+    $errors[] = "Le nom est obligatoire";
+elseif (strlen($dataFinal["name"]) > 20)
+    $errors[] = "Le nom est trop long";
+else
+    $name = $dataFinal["name"];
+
+//validation Email
+if (!isset($dataFinal["email"]) || empty($dataFinal["email"]))
+    $errors[] = "Email est obligatoire";
+elseif (!filter_var($dataFinal["email"], FILTER_VALIDATE_EMAIL))
+    $errors[] = "Le mail n'est pas valide";
+else
+    $email = $dataFinal["email"];
+
+
+//Validating message
+if (!isset($dataFinal["message"]) || empty($dataFinal["message"]))
+    $errors[] = "Le champ message est obligatoire";
+else
+    $message = $dataFinal["message"];
+
+
+//Validating CGU
+if (!isset($dataFinal["cgu"]) || empty($dataFinal["cgu"]))
+    $errors[] = "Accepter les CGU est obligatoire";
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Contact Envoyé</title>
+</head>
+<body>
+<?php
+if (empty($errors)) {
+    echo "<p>Merci $name pour votre message : $message</p>";
+} else {
+    echo "<h2>Votre demande n'a pas pu etre traitée : </h2>";
+    echo "<ul>";
+    foreach ($errors as $error) {
+        echo "<li>$error</li>";
+    }
+    echo "</ul>";
 }
+?>
+
+</body>
+</html>
